365618511d3c2ac520fb6844bcaa66ef8529dab5
Otto Kestler <okestlerr@miumg.edu.gt>
2024-11-12 22:35:26 -0600

    TareaIxSpringBootApplication

---
+++ src/main/java/org/example/tareaix/TareaIxSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.tareaix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaIxSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareaIxSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tareaix/TareaIxSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tareaix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaIxSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
